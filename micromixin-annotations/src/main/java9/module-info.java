d76bdaab428dc607f27a42afb1e6c9d9a668d459
Geolykt <admin@geolykt.de>
2025-09-04 15:51:24 +0200

    Bump version, minor JPMS fixes

--- micromixin-annotations/src/main/java9/module-info.java
+++ micromixin-annotations/src/main/java9/module-info.java
@@ -1,7 +1,6 @@
 module org.stianloader.micromixin.annotations {
     requires org.jetbrains.annotations;
 
-    exports org.spongepowered.asm.mixin.injection.callback;
     exports org.spongepowered.asm.mixin.injection;
     exports org.spongepowered.asm.mixin;
     exports org.stianloader.micromixin.annotations;
